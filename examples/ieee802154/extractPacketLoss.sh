81a02b0bc7fcd451398113dcbeca31fe0a73f4e3
tonsense <tonsense@idelight.de>
2015-04-13 17:34:00 +0200

    added packet loss

---
+++ examples/ieee802154/extractPacketLoss.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+numPackets=100
+scaFile=GeographicDSME-0.sca
+
+cd results
+
+grep rcvdPkSrc GeographicDSME-0.sca | sed 's/.*:host\[/scalar Net802154Geographic.host[/' | sed 's/.wlan\[.\]/.trafficgen\tdestRcvdPk/' >> $scaFile
