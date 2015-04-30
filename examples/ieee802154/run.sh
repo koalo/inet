09064a2adab9d82b32786ff5896de40e867ea7c4
tonsense <tonsense@idelight.de>
2015-04-30 18:28:18 +0200

    some more runs

--- examples/ieee802154/run.sh
+++ examples/ieee802154/run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-opp_run -r 1,2 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET --repeat=2 -f omnetpp.ini
+opp_run -r 0..11 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+opp_run -r 12..23 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+opp_run -r 24..35 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+opp_run -r 36..47 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+
+
+exit
+
+#TODO do when oppruns done
+for i in {0..47}
+do
+  ./extractPacketLoss.sh DSME- $i
+done
