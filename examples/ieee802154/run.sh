64e37e9564cf3985d365fa9f2e83419408c537c9
tonsense <tonsense@idelight.de>
2015-05-11 09:29:07 +0200

    analysis file differ queue

--- examples/ieee802154/run.sh
+++ examples/ieee802154/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-opp_run -r 0..55 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
-opp_run -r 56..127 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
-opp_run -r 128..167 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
-#opp_run -r 36..47 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+opp_run -r 0..41 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+opp_run -r 42..83 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+opp_run -r 84..125 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
+opp_run -r 126..167 -u Cmdenv -c GeographicDSME -n ..:../../src -l ../../src/INET -f omnetpp.ini >/dev/null &
 
 
 exit
